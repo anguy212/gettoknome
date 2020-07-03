d9bab394563aba6f615bf2565665dd90061e5c9b
anguy212 <32521966+anguy212@users.noreply.github.com>
2020-07-03 05:46:52 -0700

    Updates

---
+++ precache-manifest.c6b85ffd73fcc3595cb74365afb9cf92.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78a420a37cdc4b67177bdb2eb1552621",
+    "url": "/gettoknome/index.html"
+  },
+  {
+    "revision": "41ca2bc92dfa1f1f78ae",
+    "url": "/gettoknome/static/css/main.9c21797f.chunk.css"
+  },
+  {
+    "revision": "ee77c1a71d8b7b8aa26b",
+    "url": "/gettoknome/static/js/2.5af4c248.chunk.js"
+  },
+  {
+    "revision": "5eeaef24d459591afeaec501ac57b571",
+    "url": "/gettoknome/static/js/2.5af4c248.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41ca2bc92dfa1f1f78ae",
+    "url": "/gettoknome/static/js/main.1b0e7635.chunk.js"
+  },
+  {
+    "revision": "9689b6459d2ffca68076",
+    "url": "/gettoknome/static/js/runtime-main.e7cd446d.js"
+  },
+  {
+    "revision": "696e12ec212e90cd4adcaf55f993a758",
+    "url": "/gettoknome/static/media/AboutMeIcon.696e12ec.png"
+  },
+  {
+    "revision": "809f1bd5a5059658b3918dc880f71c3e",
+    "url": "/gettoknome/static/media/Circle.809f1bd5.png"
+  },
+  {
+    "revision": "309b45bc2e5a1e97665e43a51b515d22",
+    "url": "/gettoknome/static/media/ContactIcon.309b45bc.png"
+  },
+  {
+    "revision": "d327de038ff359caa205e832ca27836a",
+    "url": "/gettoknome/static/media/Education.d327de03.png"
+  },
+  {
+    "revision": "cfe9b5415dc9fdb540a223d6c472bea8",
+    "url": "/gettoknome/static/media/Hobbies.cfe9b541.png"
+  },
+  {
+    "revision": "c6c73cf8d0cd31a2b0f2cce4408cef13",
+    "url": "/gettoknome/static/media/IdeaIcon.c6c73cf8.png"
+  },
+  {
+    "revision": "e38eb112cec9e0e27216e263ccd5ead9",
+    "url": "/gettoknome/static/media/Interests.e38eb112.png"
+  },
+  {
+    "revision": "32b4bf639a9b63f28af5d9728f952b0d",
+    "url": "/gettoknome/static/media/NguyenAliceResume.32b4bf63.pdf"
+  },
+  {
+    "revision": "510c14ef0d84144e6bbcb68b531c85c3",
+    "url": "/gettoknome/static/media/ResumeIcon.510c14ef.png"
+  },
+  {
+    "revision": "77d488f5b5fd0346b884dd429c30d72a",
+    "url": "/gettoknome/static/media/SafeWalk.77d488f5.png"
+  },
+  {
+    "revision": "f8bd9ef1e4f4460493c8ce83d709558a",
+    "url": "/gettoknome/static/media/Sarcix.f8bd9ef1.png"
+  },
+  {
+    "revision": "b1870d33f15935ed5cf6119efaf79d07",
+    "url": "/gettoknome/static/media/WebCrawler.b1870d33.png"
+  }
+]);
